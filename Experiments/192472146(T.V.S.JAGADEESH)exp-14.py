6357ab806470e5dd6d61409f0a38484129586045
saijagadeesh-88 <192472146.simats@saveetha.com>
2026-08-19 11:32:06 +0530

    Update 192472146(T.V.S.JAGADEESH)exp-14.py

--- Experiments/192472146(T.V.S.JAGADEESH)exp-14.py
+++ Experiments/192472146(T.V.S.JAGADEESH)exp-14.py
@@ -1,3 +1,5 @@
+# Question: Identify any potential correlation between study time and exam scores and explore various
+# plotting functions to visualize this relationship effectively.
 import pandas as pd
 import matplotlib.pyplot as plt
 
