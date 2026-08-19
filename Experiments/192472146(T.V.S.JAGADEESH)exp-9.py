55ce15ff0e2ed48272c3e9b7b019a0dc60ec6c81
saijagadeesh-88 <192472146.simats@saveetha.com>
2026-08-19 11:44:05 +0530

    Update 192472146(T.V.S.JAGADEESH)exp-9.py

--- Experiments/192472146(T.V.S.JAGADEESH)exp-9.py
+++ Experiments/192472146(T.V.S.JAGADEESH)exp-9.py
@@ -1,3 +1,8 @@
+# Question: Using Pandas DataFrame operations, how would you find the following information from
+# the property_data DataFrame:
+# 1. The average listing price of properties in each location.
+# 2. The number of properties with more than four bedrooms.
+# 3. The property with the largest area.
 import pandas as pd
 
 df = pd.read_csv("Q9.csv")
