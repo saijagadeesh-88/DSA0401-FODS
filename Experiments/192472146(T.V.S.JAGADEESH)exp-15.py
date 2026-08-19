73a2f320583d533d4cc236cb1886e24336f571fc
saijagadeesh-88 <192472146.simats@saveetha.com>
2026-08-19 11:33:50 +0530

    Update 192472146(T.V.S.JAGADEESH)exp-15.py

--- Experiments/192472146(T.V.S.JAGADEESH)exp-15.py
+++ Experiments/192472146(T.V.S.JAGADEESH)exp-15.py
@@ -1,3 +1,10 @@
+# Question: Write a python program will take in a dataset containing daily temperature readings for each
+# city over a year and perform the following tasks:
+# 1. Calculate the mean temperature for each city.
+# 2. Calculate the standard deviation of temperature for each city.
+# 3. Determine the city with the highest temperature range (difference between the highest and lowest
+# temperatures).
+# 4. Find the city with the most consistent temperature (the lowest standard deviation).
 import pandas as pd
 import matplotlib.pyplot as plt
 
