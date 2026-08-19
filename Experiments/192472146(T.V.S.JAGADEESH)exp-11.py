18c9861d3ffc520ae38968d98bb4ce6ceff32048
saijagadeesh-88 <192472146.simats@saveetha.com>
2026-08-19 11:29:47 +0530

    Update 192472146(T.V.S.JAGADEESH)exp-11.py

--- Experiments/192472146(T.V.S.JAGADEESH)exp-11.py
+++ Experiments/192472146(T.V.S.JAGADEESH)exp-11.py
@@ -1,4 +1,9 @@
-import pandas as pd
+# Question:
+# 1. Write code to create a simple line plot in Python using Matplotlib to predict sales happened in a
+# month?
+# 2. Write code to create a scatter plot in Python using Matplotlib to predict sales happened in a month?
+# 3. Develop a Python program to create a bar plot of the monthly sales data.
+# import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv("Q10 11.csv")
