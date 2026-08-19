5c8c9dd500122557e00a183c291814991979f07f
saijagadeesh-88 <192472146.simats@saveetha.com>
2026-08-19 11:30:41 +0530

    Update 192472146(T.V.S.JAGADEESH)exp-12.py

--- Experiments/192472146(T.V.S.JAGADEESH)exp-12.py
+++ Experiments/192472146(T.V.S.JAGADEESH)exp-12.py
@@ -1,3 +1,7 @@
+# Question:
+# 1. Develop a Python program to create a line plot of the monthly temperature data.
+# 2: Develop a Python program to create a scatter plot of the monthly rainfall data.
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
