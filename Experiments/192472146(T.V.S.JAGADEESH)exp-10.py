3bb972293e449a28331ee133ff2db53b663b076d
saijagadeesh-88 <192472146.simats@saveetha.com>
2026-08-19 11:28:50 +0530

    Update 192472146(T.V.S.JAGADEESH)exp-10.py

--- Experiments/192472146(T.V.S.JAGADEESH)exp-10.py
+++ Experiments/192472146(T.V.S.JAGADEESH)exp-10.py
@@ -1,3 +1,6 @@
+# Question:
+# 1. How would you develop a Python program to create a line plot of the monthly sales data?
+# 2: How would you develop a Python program to create a bar plot of the monthly sales data?
 import pandas as pd
 import matplotlib.pyplot as plt
 
