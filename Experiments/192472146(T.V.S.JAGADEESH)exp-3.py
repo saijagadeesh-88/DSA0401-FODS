f4838b9950ff2348cf63664c17882310b689e17a
saijagadeesh-88 <192472146.simats@saveetha.com>
2026-08-19 11:38:14 +0530

    Update 192472146(T.V.S.JAGADEESH)exp-3.py

--- Experiments/192472146(T.V.S.JAGADEESH)exp-3.py
+++ Experiments/192472146(T.V.S.JAGADEESH)exp-3.py
@@ -1,3 +1,5 @@
+# Question: Using NumPy arrays and operations, how would you find the average sale price of houses
+# with more than four bedrooms in the neighborhood?
 import pandas as pd
 
 df = pd.read_csv("Q3.csv")
