81ef4d0ec42032582ec92f075bc8d2ea426a51e8
saijagadeesh-88 <192472146.simats@saveetha.com>
2026-08-19 11:39:38 +0530

    Update 192472146(T.V.S.JAGADEESH)exp-5.py

--- Experiments/192472146(T.V.S.JAGADEESH)exp-5.py
+++ Experiments/192472146(T.V.S.JAGADEESH)exp-5.py
@@ -1,3 +1,5 @@
+# Question: How would you use NumPy arrays and arithmetic operations to calculate the average fuel
+# efficiency and determine the percentage improvement in fuel efficiency between two car models?
 import numpy as np
 
 fuel_efficiency = np.array([20, 25, 30, 35])
