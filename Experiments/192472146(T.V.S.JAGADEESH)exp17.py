a59942272441a319c8896241f8c9f6ccc492fc15
saijagadeesh-88 <192472146.simats@saveetha.com>
2026-08-19 11:47:31 +0530

    Update 192472146(T.V.S.JAGADEESH)exp17.py

--- Experiments/192472146(T.V.S.JAGADEESH)exp17.py
+++ Experiments/192472146(T.V.S.JAGADEESH)exp17.py
@@ -1,3 +1,5 @@
+# Question: Develop a code in python to find the frequency distribution of the ages of the customers who
+# have made a purchase in the past month.
 import pandas as pd
 
 df = pd.read_csv("Q17.csv")
