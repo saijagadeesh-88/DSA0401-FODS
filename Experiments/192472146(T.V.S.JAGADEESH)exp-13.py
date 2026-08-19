8ba44451e03496ac43a5a77f576cff34a503ddf2
saijagadeesh-88 <192472146.simats@saveetha.com>
2026-08-19 11:31:22 +0530

    Update 192472146(T.V.S.JAGADEESH)exp-13.py

--- Experiments/192472146(T.V.S.JAGADEESH)exp-13.py
+++ Experiments/192472146(T.V.S.JAGADEESH)exp-13.py
@@ -1,3 +1,5 @@
+# Question: Your task is to build a Python program that reads the stock data from a CSV file, calculates
+# the variability of stock prices, and provides insights into the stock's price movements.
 import pandas as pd
 import matplotlib.pyplot as plt
 
