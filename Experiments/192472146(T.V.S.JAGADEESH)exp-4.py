d8d43ab973c3545207c070491621c04986085df7
saijagadeesh-88 <192472146.simats@saveetha.com>
2026-08-19 11:38:55 +0530

    Update 192472146(T.V.S.JAGADEESH)exp-4.py

--- Experiments/192472146(T.V.S.JAGADEESH)exp-4.py
+++ Experiments/192472146(T.V.S.JAGADEESH)exp-4.py
@@ -1,3 +1,5 @@
+# Question: Using NumPy arrays and arithmetic operations calculate the total sales for the year and
+# determine the percentage increase in sales from the first quarter to the fourth quarter?
 import numpy as np
 
 sales_data = np.array([10000, 15000, 20000, 25000])
